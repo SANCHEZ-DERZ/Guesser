90acd168866e8898c87ee2156bfd240afdc9b892
Alexander <derznovenko2005@mail.ru>
2025-05-09 00:47:47 +0300

    new app

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import FlagGame from './components/FlagGame';
-import './assets/styles/App.css';
-
-function App() {
-  return <FlagGame />;
-}
-
-export default App;
--- src/smartapp/assistant.js
+++
@@ -1,8 +0,0 @@
-import { createSmartappDebugger, createAssistant } from '@salutejs/client';
-
-export const initializeAssistant = (getState) => {
-  if (process.env.NODE_ENV === 'development') {
-    return createSmartappDebugger({ token: process.env.REACT_APP_TOKEN, initPhrase: 'запусти игру с флагами' });
-  }
-  return createAssistant({ getState });
-};
